3990db6298f84ff08085fe47ffb847d050ee66e4
SundousKanaan <94317411+SundousKanaan@users.noreply.github.com>
2024-12-05 02:30:39 +0100

    feat: navBar + article 1 + footer

---
+++ javascript/javascript.js
@@ -0,0 +1,37 @@
+const openNavButton = document.querySelector("#openNav");
+const nav = document.querySelector("header nav");
+
+function openNav() {
+  nav.classList.toggle("open");
+  openNavButton.classList.toggle("openNav");
+}
+
+openNavButton.addEventListener("click", openNav);
+
+const navLinks = document.querySelectorAll("header nav a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", getTarget);
+});
+
+function getTarget() {
+  navLinks.forEach((navLink) => navLink.classList.remove("selected"));
+  this.classList.add("selected");
+}
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  navLinks.forEach((link) => {
+    const section = document.querySelector(link.hash);
+    const sectionTop = section.offsetTop;
+    console.log(current);
+    if (scrollY >= sectionTop - section.offsetHeight / 3) {
+      current = link.hash;
+    }
+  });
+  navLinks.forEach((link) => {
+    link.classList.remove("selected");
+    if (link.hash === current) {
+      link.classList.add("selected");
+    }
+  });
+});
